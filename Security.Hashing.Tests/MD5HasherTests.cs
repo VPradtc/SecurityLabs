ed2dea3c3a0b0dfafe37ca366d34bf33cc3edb64
VPradtc <viacheslav.pradchuk@gmail.com>
2017-12-25 15:05:25 +0200

    Lab2 trunk

---
+++ Security.Hashing.Tests/MD5HasherTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Security.Hashing.HashGen.MD5;
+
+namespace Security.Hashing.Tests
+{
+    [TestClass]
+    public class MD5HasherTests
+    {
+        private MD5Hasher _hasher;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _hasher = new MD5Hasher();
+        }
+
+        [TestMethod]
+        public void TestEmptyString()
+        {
+            string input = String.Empty;
+            string inputHash = "D41D8CD98F00B204E9800998ECF8427E";
+
+            string hash = _hasher.Hash(input);
+
+            Assert.AreEqual(inputHash, hash);
+        }
+    }
+}
---
+++ Security.Hashing/HashGen/MD5/MD5Hasher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Security.Hashing.HashGen.MD5
+{
+    public class MD5Hasher : IHasher
+    {
+        public string Hash(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
